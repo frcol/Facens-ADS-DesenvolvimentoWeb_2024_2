bf0708a9d25cf59421acdc5a25ef49efdb21854d
Fabio Colombini <fabiorcolombini@gmail.com>
2024-10-29 19:47:58 -0300

    1

--- Aula8_MongoDB/app.js
+++ Aula8_MongoDB/app.js
@@ -1,11 +1,26 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
 
 app.use(express.json());
 
+// ====================================
 const usuarioController = require('./controllers/usuarioController');
 app.use('/usuarios', usuarioController);
 
+// ====================================
+mongoose.connect('mongodb://127.0.0.1:27017/aula10')
+    .then(() => {
+        app.listen(27017, () => {
+            console.log('Conectado ao mongoDB');
+            console.log('Servidor iniciado na porta 27017');
+        })
+    })
+    .catch((err) => {
+        console.log(err);
+    });
+
+// ====================================
 app.listen(3000, () => { 
     console.log('Server running on port 3000');
 });
