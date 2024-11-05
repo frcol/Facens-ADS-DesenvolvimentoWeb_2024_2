a27aa9c71ba7824740a7eb6c14bd4b3844cebf32
Fabio Colombini <fabiorcolombini@gmail.com>
2024-11-05 20:14:18 -0300

    Aula9

---
+++ Aula9_Services/src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { provideHttpClient } from '@angular/common/http';
+import { ProdutoComponent } from './components/produto/produto.component';
+import { FormsModule } from '@angular/forms';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ProdutoComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule
+  ],
+  providers: [
+    provideHttpClient()
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
